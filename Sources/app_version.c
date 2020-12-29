c4792101175ad174b586a2bf8addd00b82a46320
Jacques Supcik <jacque@supcik.net>
2020-12-29 04:17:53 -0800

    wip

--- Sources/app_version.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Makefile utility, please do not edit here. Edit the Makefile instead
-//
-#define VERSION_SECT  __attribute__ (( section (".version")))
-VERSION_SECT const char app_version[]="ALG0900";
